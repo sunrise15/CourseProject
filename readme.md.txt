38d7f2a7e345e163a3ff5128e843e692a0c16ebe
sunrise15 <sophiechua15@gmail.com>
2015-02-16 09:35:02 -0800

    Getting and Cleaning Data course project

---
+++ readme.md.txt
@@ -0,0 +1,20 @@
+************
+Background:
+************
+A series of experiments were made to monitor sensor signals by a group of 30 volunteers based on their 
+activities (walking, walking upstairs, walking downstairs, sitting, standing, laying).  A full descripton is 
+available at the uci website where the raw data was obtained: 
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+A Tidy data set was created based on further analysis of the raw data.  Read codebook.md for further 
+explanation of the data transformations.
+
+*******************************
+This Repo includes the following files:
+*******************************
+Readme.md – this document
+Tidydata.txt – a tidy data set
+Codebook.md – codebook that describes the variables, the data, and process of transformation from 
+raw data to the tidy data set
+Run_analysis.R – the R code that reads the raw data produced by the initial experiments and transforms 
+it into the tidy data set
--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,23 @@
+##################################################################
+##################################################################
+# This function does analysis on the data gathered from various 
+# experiments that monitor sensor signals based on people's activities.
+##################################################################
+##################################################################
 
 analysis <- function() {
 	
-	# merge the training and test data files together 	
-	dat <- data.frame()
+	###########
+	# Read files
+	###########
+
+	# read measurement files 	
+	measure <- data.frame()
 	x <- read.table("./UCI HAR Dataset/train/X_train.txt")
-	dat <- rbind(dat,x)
+	measure <- rbind(measure,x)
 	x <- read.table("./UCI HAR Dataset/test/X_test.txt")
-	dat <- rbind(dat,x)
-	numrowdat<-nrow(dat)
+	measure <- rbind(measure,x)
+	numrowmeas<-nrow(measure)
 
 	# read activity files 	
 	activity <- data.frame()
@@ -15,138 +25,84 @@ analysis <- function() {
 	activity <- rbind(activity,x)
 	x <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 	activity <- rbind(activity,x)
+	numrowactivity <- nrow(activity)
 	
+	# read subject files 	
+	subject <- data.frame()
+	x <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+	subject <- rbind(subject,x)
+	x <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+	subject <- rbind(subject,x)
+	numrowsubject <- nrow(subject)
+
 	#read feature table
 	feature <- read.table("./UCI HAR Dataset/features.txt")
 	numrowfeat <- nrow(feature)
 	feature$V2 <- as.character(feature$V2)
 	
-	#create new feature and data files 
-	newfeat <- data.frame(c1=numeric(0),c2=character(0))
-	newdat<-data.frame(matrix(nrow=numrowdat,ncol=0))
+	#read activity labels table
+	labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+	labels$V2 <- as.character(labels$V2)
+	labels$V2 <- tolower(labels$V2)
+	
+	###########
+	# Create data file
+	###########
+
+	#create data file column subject
+	newdat<-subject
+	names(newdat)[1] <- "Subject"
+
+	#add data file column activity
 	newact<-data.frame()
+	
+	for (i in 1:numrowactivity){
+		actnum <- activity[i,]
+		label1<-labels[which(labels[,"V1"] == actnum),]
+		text <- label1[1,2]
+		newact<-rbind(newact,text)
+		newact[,1]<-as.character(newact[,1])
+	}
+	newdat <- cbind(newdat,newact)
+	names(newdat)[2] <- "Activity"
+
+	# add data file columns for measurements
+	# only pick the mean and standard deviation columns
 
 	for (i in 1:numrowfeat){
-		
-		# is mean() or std() in the feature table?
-		
+	
 		texttosearch <- feature[i,2]		
-		isthere <- grep("mean()", texttosearch)
-		if (length(isthere) == 0){
-			isthere <- grep("std()", texttosearch)
-			}
+		ismean <- grep("mean\\(\\)", texttosearch)
+		isstd<-grep("std\\(\\)", texttosearch)
 		
-		# if mean() or std() found, then add to new tables
-
-		if (length(isthere) == 1) {
-			
-			#new feature table
-			onef<- cbind(i,texttosearch)
-			names(onef)<-names(newfeat)
-			newfeat <- rbind(newfeat,onef)
-			
+		# if mean or std found, then add to new tables
+		if ((length(ismean) == 1) | (length(isstd) == 1 )){
+						
     			# new data table
-			oned <- dat[,i]
+			oned <- measure[,i]
 			newdat <- cbind(newdat,oned)
 			numcol <- ncol(newdat)
-			names(newdat)[numcol] <- texttosearch
+			hdr<-gsub("\\(\\)","",texttosearch)
+			hdr<-gsub("-",".",hdr)
+			names(newdat)[numcol] <- hdr
 
-			# new activity table
-			onea<-activity[i,]
-			newact <- rbind(newact,onea)
-			}
+		}
 	}
 	
-	#create descriptive activity names for the data set
-	oned2<-data.frame(0)
-	oned2<-data.frame(lapply(oned2,as.character),stringsAsFactors=FALSE)
-	tidydf<-data.frame()
+	###########
+	# Create tidy data 
+	# uses package reshape2
+	###########
+
+	x <- newdat[order(newdat$Subject,newdat$Activity),]
+	xmelt <- melt(x,id=c("Subject","Activity"),measure.vars=c(3:numcol))
+	tidydata <- dcast(xmelt,Subject+Activity~variable,mean)
 			
-	for (j in 1:numcol){
-		colhdr<- names(newdat)[j]
-		
-		c<-substr(colhdr,0,1)	
-		if (c=="t"){
-		c1<-"Time"
-		}else if (c=="f") {
-		c1<-"Frequency"
-		}else {c1<-""}
-				
-		isgyro<-grep("Gyro",colhdr)
-		isacc<-grep("Acc",colhdr)
-		if (length(isgyro)==1){
-		c2<-"Gyroscope"
-		}else if (length(isacc)==1) {
-		c2<-"Accelerometer"
-		}else {c2<-""}
+	###########
+	# Return data
+	###########
 
-		ismean <- grep("mean()", colhdr)
-		isstd<-grep("std()", colhdr)
-		if (length(ismean) == 1){
-		c3 <- "Mean"
-		}else if (length(isstd)==1){
-		c3<-"Standard Deviation"
-		}else {c3<-"NA"}
-
-		isx <- grep("-X", colhdr)
-		isy<-grep("-Y", colhdr)
-		isz<-grep("-Z", colhdr)
-		if (length(isx) == 1){
-		c4 <- "X"
-		}else if (length(isy)==1){
-		c4<-"Y"
-		}else if (length(isz)==1){
-		c4<-"Z"
-		}else {c4<-"ALL"}
-
-		c5<-colMeans(newdat[j])
-						
-		actnum<-newact[j,]
-		if(actnum == 1){
-		c6<-"WALKING"
-		}else if(actnum==2){
-		c6<-"WALKING UPSTAIRS"
-		}else if (actnum==3){
-		c6<- "WALKING DOWNSTAIRS"
-		}else if (actnum==4){
-		c6<- "SITTING"
-		}else if (actnum==5){
-		c6<- "STANDING"
-		}else if (actnum==6){
-		c6<- "LAYING"
-		}else {c6<-""}
-		
-		#header row for newdat
-		oned2<-cbind(oned2,c6)
-		
-		#add tidy data
-		keytidy<-paste(c6,c3,c4,sep="")
-		onetidy<-cbind(keytidy,c6,c3,c4,c5)
-		tidydf<-rbind(tidydf,onetidy)
-	}
-
-	
-	#create newdat2 = newdat data frame with one row of descriptive header on top
-	numcold2<-ncol(oned2)
-	newdat2<-oned2[c(2:numcold2)]	
-	newdat2<-data.frame(lapply(newdat2,as.character),stringsAsFactors=FALSE)
-	names(newdat2)<-names(newdat)	
-	newdat2<-rbind(newdat2,newdat) 
-
-	#add column header to tidy data
-	row.names(tidydf)<-NULL
-	colnames(tidydf) <- c("Key","Activity","Variable","Subject","Average")	
-
-	#get average for each variable
-	x<-tidydf
-	x$Key<-as.vector(x$Key)
-	x$Average<-as.numeric(as.character(x$Average))
-	x1<-ddply(x,.(Key),summarize,Activity=Activity,Variable=Variable,Subject=Subject,Average=ave(Average,FUN=mean))
-	x2<-unique(x1[,1:5])
-	tidydf2<-x2[,c(2:5)]
-
-	#return data
-	return (tidydf2)
+	return (tidydata)
 }
 
 			
